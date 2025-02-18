896aa8fab70f724bfa37d73c46a1d47ea7d401e4
caroline <161317565+lpmtm@users.noreply.github.com>
2025-02-18 20:29:53 -0300

    Update main, .h, .c

--- codigos/aula1_calculadora/main, .h, .c
+++ codigos/aula1_calculadora/main, .h, .c
@@ -1,3 +1,4 @@
+//CALCULADORA.H
 #include <stdlib.h>
 #include <math.h>
 
@@ -66,3 +67,53 @@ void calculadora_divide(Calculadora *c, float x, float y){
 	float cy= c1  ->y / c2->y;
 	return sqrt(cx / cy);
 }
+
+
+//CALCULADORA.C
+#ifndef calculadora_h
+#define calculadora_h 1
+
+typedef struct calculadora Calculadora;
+
+calculadora* calculadora_cria(float x, float y);
+void calculadora_exibe(calculadora *c);
+void calculadora_zera(calculadora *c);
+void calculadora_destroi(Ponto *p);
+void calculadora_soma(Calculadora *c, float x, float y);
+void calculadora_subtrai(Calculadora *c, float x, float y);
+void calculadora_multiplica(Calculadora *c,float x, float y);
+void calculadora_divide(Calculadora *c, float x, float y);
+
+#endif
+
+//MAIN.C
+#include <stdio.h>
+#include "calculadora.h"
+
+int main() {
+    Calculadora *c1, *c2;
+    c1 = calculadora_cria(15, 5);
+    c2 = claculadora_cria(10, 35);
+    
+    float x, y;
+    
+    claculadora_exibe(c1);
+    claculadora_exibe(c2);
+    c1 = claculadora_cria(15, 5);
+    c2 = claculadora_cria(10, 35);
+    
+        float soma = calculadora_soma(c1, c2);
+    printf("a soma é: %f (%p)\n", soma, c1);
+    
+    float subtrai = calculadora_subtrai(c1, c2);
+    printf("a subtraçao é: %f (%p)\n", subtrai, c1);
+    
+    float multiplica = calculadora_multiplica(c1, c2);
+    printf("a multiplicacao é: %f (%p)\n", multiplica, c1);
+    
+    float divide = calculadora_divide(c1, c2);
+    printf("a divisao é: %f (%p)\n", divide, c1);
+
+    return 0;
+}
+	
