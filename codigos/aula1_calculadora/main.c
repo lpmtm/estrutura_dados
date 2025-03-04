206acfae4b8db8037aa2315da62f15845c490a49
caroline <161317565+lpmtm@users.noreply.github.com>
2025-03-04 19:46:49 -0300

    Update main.c

--- codigos/aula1_calculadora/main.c
+++ codigos/aula1_calculadora/main.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
-#include "calculadora.h"
+
+#include "Calculadora.h"
 
 int main() {
-    Calculadora *c1, *c2;
-    c1 = calculadora_cria(15, 5);
-    c2 = claculadora_cria(10, 35);
-    
-    float x, y;
-    
-    claculadora_exibe(c1);
-    claculadora_exibe(c2);
-    c1 = claculadora_cria(15, 5);
-    c2 = claculadora_cria(10, 35);
-    
-        float soma = calculadora_soma(c1, c2);
-    printf("a soma é: %f (%p)\n", soma, c1);
-    
-    float subtrai = calculadora_subtrai(c1, c2);
-    printf("a subtraçao é: %f (%p)\n", subtrai, c1);
-    
-    float multiplica = calculadora_multiplica(c1, c2);
-    printf("a multiplicacao é: %f (%p)\n", multiplica, c1);
-    
-    float divide = calculadora_divide(c1, c2);
-    printf("a divisao é: %f (%p)\n", divide, c1);
-
+    Calculadora *c = criar();
+    exibir(c);
+    somar(c, 10);
+    exibir(c);
+    subtrair(c, 5);
+    exibir(c);
+    multiplicar(c, 2);
+    exibir(c);
+    dividir(c, 4);
+    exibir(c);
+    zerar(c);
+    exibir(c);
+    destruir(c);
     return 0;
 }
