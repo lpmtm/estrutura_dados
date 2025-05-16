1cd3b1f61e2dcbb9e3db1d9b8f6d72e79354c91e
caroline lopes <161317565+lpmtm@users.noreply.github.com>
2025-05-16 13:40:03 +0000

    atividade

--- codigos/arvores/arvores_binarias_at.c
+++ codigos/arvores/arvores_binarias_at.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct no No;
+
 struct no {
-int dado;
-No *esq;
+    int dado;
+    No *esq;
 };
+
+void inserir_ordenado(No *arvore, int dado){
+
+}
